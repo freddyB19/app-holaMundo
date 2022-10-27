f555ad71e3c8556f518a7cd6c197cd02c7a982e6
freddyB19 <freddyybolivar2013@gmail.com>
2022-10-27 10:26:51 -0400

    updated messagess_header

--- src/utils/functions/messagess_header.js
+++ src/utils/functions/messagess_header.js
@@ -1,9 +1,16 @@
 const messagess_header = () => {
   const messagess = [
-    `Multiple lines of text that form the lede.`,
-    `informing new readers quickly and efficiently.`,
-    `about what’s most interesting in this post’s contents.`,
-    `dadasdasdasdas`,
+    `Un programador al nacer no llora, dice 'Hola Mundo'`,
+    `Para ser un buen programador se requiere 3% de talento y un 97% de esfuerzo para NO DISTRARSE CON EL INTERNET`,
+    `La tecnología permitirá hackear a los seres humanos. -- Yuval Noah`,
+    `Cuando escribí este código, solo Dios y yo sabiamos cómo y para qué lo hice. Ahora solo Dios lo sabe.`,
+    `¿Sabes cuál es el problema? Imaginarte el algoritmo y no programarlo.`,
+    `No hay lugar como 127.0.0.1`,
+    `Si al principio no tienes exito, llámale 'Versión 1.0'`,
+    `Hay 10 tipos de personas en este mundo, las que entienden binario y las que no.`,
+    `Una imagen vale más que mil palabras. Pero ocupa mucho más memoria`,
+    `No gastes 6 minutos en hacer algo a mano cuando puedes gastar 6 horas fallando en automatizarlo.`,
+    `La vida seria más sencilla si pudieramos echar un vistaso al código fuente.`
   ]
 
   return messagess[Math.floor(Math.random() * messagess.length)];
